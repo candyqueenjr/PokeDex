afa4b1b60fcec54b5685b8507930b15f11e6be3b
candyqueenjr <vardosanidzeano@gmail.com>
2022-11-21 04:53:27 +0400

    რაღაც არ გამოვიდა ps. გადაწერილი კოდიც კი არმუშაობს ჩემ ხელში

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ const pokemon_count = 150
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
-    electric: 'FCF7DE',
-    water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
-    flying: '#F5F5F5',
-    fighting: '#E6E0D4',
-    normal: '#D5D5D5'
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
 }
 
-const fetchPokemons = () => {
+const main_types = Object.keys(colors)
+
+const fetchPokemons = async () => {
     for(let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
     }
@@ -34,18 +36,29 @@ const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div')
     pokemonEl.classList.add('pokemon')
 
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
     const pokemonInnerHTML = `
-        <div class="img-container">
-          <img src="src/img/1.png" />
-        </div>
-        <div class="info">
-          <span class="number">#001</span>
-          <h3 class="name">Bulbasaur</h3>
-          <small class="type">Type: <span>grass</span></small>
-        </div>
+    <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
+    </div>
     `
 
-    
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokemonEl)
 }
 
 fetchPokemons()
